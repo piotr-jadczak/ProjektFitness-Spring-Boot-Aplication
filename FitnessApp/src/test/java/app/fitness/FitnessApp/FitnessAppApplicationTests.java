c7d2d92b1141b0302fa9015fe61999e258bd85d9
piotr-jadczak <58400623+piotr-jadczak@users.noreply.github.com>
2021-11-07 18:10:42 +0100

    add project build

---
+++ FitnessApp/src/test/java/app/fitness/FitnessApp/FitnessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package app.fitness.FitnessApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
